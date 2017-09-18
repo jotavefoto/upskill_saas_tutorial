2398da7da04faa38163e4be05a3a62a036a91f0f
Joaquin Villaverde <jotavefoto@gmail.com>
2017-09-18 18:15:59 +0000

    Added custom contact form path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   resources :contacts  # crea muchos routes para create, edit, view, destroy
+  get 'contact-us', to: 'contacts#new'
 end
